7715f200957a672a26644e4b333d676979acae47
Fulvio Ieva <--replace-all>
2016-09-02 18:15:22 +0200

    add

--- telbot.php
+++ telbot.php
@@ -6,34 +6,42 @@ define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chatID = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+$userId = $update["message"]["from"]["id"];
+
 		
 // compose reply
 $reply =  sendMessage();
 		
 // send reply
-$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
-file_get_contents($sendto);
-$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$update["message"]["text"];
-file_get_contents($sendto);
-$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$update["message"]["from"]["id"];
-file_get_contents($sendto);
-// send photo
+if ($userId == 233490624) {
+	$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+	file_get_contents($sendto);
+	$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$message;
+	file_get_contents($sendto);
+	$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$userId;
+	file_get_contents($sendto);
+	// send photo
 
-$url        = API_URL."sendPhoto?chat_id=" . $chatID ;
-$post_fields = array('chat_id'   => $chat_id,
-    'photo'     => new CURLFile(realpath("amiga.png"))
-);
+	$url        = API_URL."sendPhoto?chat_id=" . $chatID ;
+	$post_fields = array('chat_id'   => $chat_id,
+	    'photo'     => new CURLFile(realpath("amiga.png"))
+	);
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Content-Type:multipart/form-data"
-));
-curl_setopt($ch, CURLOPT_URL, $url); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields); 
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	    "Content-Type:multipart/form-data"
+	));
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields); 
 
-$output = curl_exec($ch);
+	$output = curl_exec($ch);
+}else{
+	$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+	file_get_contents($sendto);
 
+}
 
 
 
@@ -41,7 +49,7 @@ $output = curl_exec($ch);
 		$date = getdate();
 		$ore = $date[hours];
 		$minuti = $date[minutes];
-		//if (strlen($minuti)==1) $minuti .= '0'.$minuti;
+		if (strlen($minuti)==1) $minuti .= '0'.$date[minutes];
 		$message = "Benvenuto al Mupin sono le ".$ore.":".$minuti;
 		
 		return $message;
